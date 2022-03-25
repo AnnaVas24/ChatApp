86ca2038e7f131a35f1c1223d871dedeb374c5ea
Anna <anna.vasi4ko@icloud.com>
2022-03-25 20:17:58 +0300

    adding message kit

---
+++ ChatApp/Controllers/ChatsViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ChatsViewController.swift
+//  ChatApp
+//
+//  Created by Vasichko Anna on 25.03.2022.
+//
+
+import UIKit
+import MessageKit
+
+class ChatsViewController: MessagesViewController {
+    
+    private var messages = [MMessage]()
+    private let user: MUser
+    private let chat: MChat
+    
+    init(user: MUser, chat: MChat) {
+        self.user = user
+        self.chat = chat
+        super.init(nibName: nil, bundle: nil)
+        
+        title = chat.friendUsername
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cofigureMessageInputBar()
+
+        
+    }
+    
+    func cofigureMessageInputBar(){
+        messageInputBar.isTranslucent = true
+        messageInputBar.separatorLine.isHidden = true
+        messageInputBar.backgroundView.backgroundColor = .mainWhite()
+        messageInputBar.inputTextView.backgroundColor = .white
+        messageInputBar.inputTextView.placeholderTextColor = .lightGray
+        messageInputBar.inputTextView.textContainerInset = UIEdgeInsets(top: 14, left: 30, bottom: 14, right: 36)
+        messageInputBar.inputTextView.placeholderLabelInsets = UIEdgeInsets(top: 14, left: 36, bottom: 14, right: 36)
+        messageInputBar.inputTextView.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.1530960874)
+        messageInputBar.inputTextView.layer.borderWidth = 0.2
+        messageInputBar.inputTextView.layer.cornerRadius = 20
+        messageInputBar.inputTextView.layer.masksToBounds = true
+        messageInputBar.inputTextView.scrollIndicatorInsets = UIEdgeInsets(top: 14, left: 0, bottom: 14, right: 0)
+        
+        messageInputBar.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.7970019113)
+        messageInputBar.layer.shadowRadius = 4
+        messageInputBar.layer.shadowOpacity = 0.3
+        messageInputBar.layer.shadowOffset = CGSize(width: 0, height: 4)
+        
+        configureSendButton()
+    }
+    
+    func configureSendButton(){
+        messageInputBar.sendButton.setImage(UIImage(named: "Sent"), for: .normal)
+        messageInputBar.sendButton.applyGradient(cornerRadius: 10)
+        messageInputBar.setRightStackViewWidthConstant(to: 56, animated: false)
+        messageInputBar.sendButton.contentEdgeInsets = UIEdgeInsets(top: 2, left: 2, bottom: 6, right: 30)
+        messageInputBar.sendButton.setSize(CGSize(width: 48, height: 48), animated: false)
+        messageInputBar.middleContentViewPadding.right = -38
+    }
+
+}
+
+extension ChatsViewController: MessagesDataSource {
+    func currentSender() -> SenderType {
+        Sender(senderId: user.id, displayName: user.username)
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        messages[indexPath.item]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        1
+    }
+    
+    func numberOfItems(inSection section: Int, in messagesCollectionView: MessagesCollectionView) -> Int {
+        messages.count
+    }
+    
+    
+}
--- ChatApp/Services/Models/MMessage.swift
+++ ChatApp/Services/Models/MMessage.swift
@@ -7,42 +7,56 @@
 
 import UIKit
 import FirebaseFirestore
+import MessageKit
 
-struct MMessage: Hashable {
+struct MMessage: Hashable, MessageType {
+    
     let content: String
-    let senderUsername: String
-    let senderId: String
     let sentDate: Date
     let id: String?
+    
+    var sender: SenderType
+    var messageId: String {
+        id ?? UUID().uuidString
+    }
+    var kind: MessageKind {
+        .text(content)
+    }
     
     var representation: [String: Any] {
         let rep: [String: Any] = [
             "created": sentDate,
-            "senderId": senderId,
+            "senderId": sender.senderId,
             "content": content,
-            "senderUsername": senderUsername
+            "senderUsername": sender.displayName
         ]
         return rep
     }
     init?(document: QueryDocumentSnapshot) {
         let data = document.data()
         guard let content = data["content"] as? String,
-        let senderUsername = data["senderUsername"] as? String,
-        let senderId = data["senderId"] as? String,
+              let senderUsername = data["senderUsername"] as? String,
+              let senderId = data["senderId"] as? String,
         let sentDate = data["created"] as? Timestamp else {return nil}
         
         self.content = content
-        self.senderUsername = senderUsername
-        self.senderId = senderId
         self.sentDate = sentDate.dateValue()
         self.id = document.documentID
+        self.sender = Sender(senderId: senderId, displayName: senderUsername)
     }
     
     init(user: MUser, content: String) {
         self.content = content
-        senderId = user.id
-        senderUsername = user.username
+        sender = Sender(senderId: user.id, displayName: user.username)
         sentDate = Date()
         id = nil
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(messageId)
+    }
+    
+    static func == (lhs: MMessage, rhs: MMessage) -> Bool {
+        lhs.messageId == rhs.messageId
     }
 }
